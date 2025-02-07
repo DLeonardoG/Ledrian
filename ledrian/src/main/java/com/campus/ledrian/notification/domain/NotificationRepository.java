a348fb5900b90f04a08c2d02b848f21d34acdc4a
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-02-07 10:12:12 -0500

    feat(Last notifications update):
    Last notifications update

--- ledrian/src/main/java/com/campus/ledrian/notification/domain/NotificationRepository.java
+++ ledrian/src/main/java/com/campus/ledrian/notification/domain/NotificationRepository.java
@@ -1,15 +1,23 @@
 package com.campus.ledrian.notification.domain;
 
 import com.campus.ledrian.notification.domain.Notification;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface NotificationRepository {
-    List<Notification> findUnreadNotificationsByUserId(Long userId);
-    List<Notification> findReadNotificationsByUserId( Long userId);
-    void markNotificationAsRead(Long notificationId);
+    @Query("SELECT n FROM Notification n WHERE n.receiver.id = :userId AND n.checked = false")
+    List<Notification> findUnreadNotificationsByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT n FROM Notification n WHERE n.receiver.id = :userId AND n.checked = true")
+    List<Notification> findReadNotificationsByUserId(@Param("userId") Long userId);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.checked = true WHERE n.id = :notificationId")
+    void markNotificationAsRead(@Param("notificationId") Long notificationId);
     Notification save(Notification notification);
     List<Notification> findAll();
     Optional<Notification> findById(Long id);
